a5c350b304dad20b29c753f73effd2b7889836c5
AravindMafia <macherlaaravind4@gmail.com>
2023-04-20 22:50:24 +0530

    product added to the cart

--- product.js
+++ product.js
@@ -1,7 +1,9 @@
 let product = document.getElementById("product");
 
 
-let arr=[
+
+
+let arr =[
     {
         "id":1,
         "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/global_images-3606000480681-1_288x288.jpg?v=1675404727",
@@ -49,61 +51,67 @@ let arr=[
         "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/global_images-5060534510043-1_525x525.jpg?v=1675977386",
         "brand" : "THE LIGHT SALON",
         "name" : "Boost LED Mask",
-        "price" : ""
+        "price" : "495"
     },
     {
         "id":8,
         "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/global_images-817244010234-1_288x288.jpg?v=1669025301",
         "brand" : "IS Clinical",
         "name" : "Cleansing Complex",
-        "price" : "45+"
+        "price" : "45"
     },
     {
         "id":9,
-        "img":"",
-        "brand" : "",
-        "name" : "",
-        "price" : ""
+        "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/global_images-635494263008-1_288x288.jpg?v=1675401099",
+        "brand" : "SkinCeuticals",
+        "name" : "C E Ferulic",
+        "price" : "182"
     },
     {
         "id":10,
-        "img":"",
-        "brand" : "",
-        "name" : "",
-        "price" : ""
+        "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/variant_images-size-1oz-747930000020-1_288x288.jpg?v=1681239872",
+        "brand" : "LA MER",
+        "name" : "Crème de La Mer Face Cream",
+        "price" : 100
     },
     {
-        "id":2,
-        "img":"",
-        "brand" : "",
-        "name" : "",
-        "price" : ""
+        "id":11,
+        "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/variant_images-size-17floz50ml-5060552901892-1_288x288.jpg?v=1674683928",
+        "brand" : "Augustinus Bader",
+        "name" : "The Rich Cream",
+        "price" : 180
     },
     {
-        "id":2,
-        "img":"",
-        "brand" : "",
-        "name" : "",
-        "price" : ""
+        "id":12,
+        "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/variant_images-size-17floz50ml-816218022976-1_288x288.jpg?v=1681234163",
+        "brand" : "Supergoop!",
+        "name" : "Unseen Sunscreen SPF 40",
+        "price" : 38
     },
     {
-        "id":2,
-        "img":"",
-        "brand" : "",
-        "name" : "",
-        "price" : ""
+        "id":13,
+        "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/global_images-9353371000003-1_288x288.jpg?v=1668764368",
+        "brand" : "ZitSticka",
+        "name" : "Killa Kit Deep Zit Microdart Patch",
+        "price" : 29
     },
     {
-        "id":2,
-        "img":"",
-        "brand" : "",
-        "name" : "",
-        "price" : ""
+        "id":14,
+        "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/global_images-866655000471-1_288x288.jpg?v=1679346440",
+        "brand" : "VINTNER'S DAUGHTER",
+        "name" : "Active Renewal Cleanser",
+        "price" : 98
+    },
+    {
+        "id":15,
+        "img":"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/global_images-812025013008-1_288x288.jpg?v=1681135311",
+        "brand" : "PCA SKIN",
+        "name" : "Intensive Brightening Treatment",
+        "price" : "117"
     }
     
 ]
 display(arr)
-
 
 // display function START ***********
 function display(data){
@@ -119,26 +127,32 @@ function display(data){
        
         let name = document.createElement("h4")
         name.textContent = element.name;
-        let rating = document.createElement("p")
-        rating.textContent = element.rating;
         let price = document.createElement("h5")
 
         price.textContent ="Rs"+" "+ element.price
-        let quantity = document.createElement("p")
-        quantity.textContent = element.quantity;
-        let inc = document.createElement("button")
-        inc.textContent = "+"
-        inc.addEventListener("click",()=>{
-            localStorage.setItem
+       
+        let btn = document.createElement("button")
+        btn.textContent = "Add To Bag"
+        btn.setAttribute("id","add")
+        btn.addEventListener("click",function(){
+                
+            let CartArr = JSON.parse(localStorage.getItem("Bag-Items"))||[]
+            CartArr.push({...element});
+            localStorage.setItem("Bag-Items",JSON.stringify(CartArr))
+            alert("Product Added To The Bag")
+            
+             
+            
         })
 
         
 
 
         
-        cart.append(img,brand,name,rating,quantity,price);
+        cart.append(img,brand,name,price,btn);
         product.append(cart)
     })
 }
 
-// display function END ***********
+
+
